dc130d08ff7bf636f9b2ae131bab51040cb17162
jesus sanchez sarmiento <xhuxho.emprende@gmail.com>
2025-01-02 11:19:06 -0600

    modelo de persona

---
+++ app/src/main/java/com/dprogramacionjg/historialtecnologico/model/Persona.java
@@ -0,0 +1,64 @@
+package com.dprogramacionjg.historialtecnologico.model;
+
+public class Persona {
+
+    public String Uid;
+    public String Nombre;
+    public String Apellidos;
+    public String NumeroTelefonico;
+    public String Correo;
+
+    public Persona() {
+    }
+
+    public String getUid() {
+        return Uid;
+    }
+
+    public void setUid(String uid) {
+        Uid = uid;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return Apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        Apellidos = apellidos;
+    }
+
+    public String getNumeroTelefonico() {
+        return NumeroTelefonico;
+    }
+
+    public void setNumeroTelefonico(String numeroTelefonico) {
+        NumeroTelefonico = numeroTelefonico;
+    }
+
+    public String getCorreo() {
+        return Correo;
+    }
+
+    public void setCorreo(String correo) {
+        Correo = correo;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "Uid='" + Uid + '\'' +
+                ", Nombre='" + Nombre + '\'' +
+                ", Apellidos='" + Apellidos + '\'' +
+                ", NumeroTelefonico='" + NumeroTelefonico + '\'' +
+                ", Correo='" + Correo + '\'' +
+                '}';
+    }
+}
